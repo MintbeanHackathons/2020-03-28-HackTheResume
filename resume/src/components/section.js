1c7f11e03a3ec404c9bf3f9d73f621e423c71576
AliceMathews <alice_mathews@hotmail.co.uk>
2020-03-28 10:45:41 -0700

    prints section titles

--- resume/src/components/section.js
+++ resume/src/components/section.js
@@ -1,10 +1,9 @@
 import React from "react";
-import resumeJSON from "../data/resume";
 
 export default function Section(props) {
   return (
     <>
-      <div className="section">{resumeJSON.basics.name}</div>
+      <div className="section">{props.title}</div>
     </>
   );
 }
