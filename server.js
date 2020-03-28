0e5d67b041869d1718c7aabcc1fcb445a00723c6
Ahmed Yagoub <ahyagoub40@gmail.com>
2020-03-28 11:30:31 -0600

    connected the server and displayed the html/view

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-
 const PORT = 3000;
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("resume");
+})
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
