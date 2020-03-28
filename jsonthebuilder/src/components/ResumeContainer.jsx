b8ce8bdd9c894bee012839c015c2446e6bcd014c
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-03-28 11:18:40 -0700

    added components to use for rendering the resume

--- jsonthebuilder/src/components/ResumeContainer.jsx
+++ jsonthebuilder/src/components/ResumeContainer.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import sampleResume from "../assets/sampleResume";
 
 export default function ResumeContainer() {
+  // const sample = {sampleResume: "peos"};
+  // const [information, setInformation] = useState(JSON.parse(sample))
+  const jasson = JSON.stringify(sampleResume);
+  const obj = [JSON.parse(jasson)];
+  console.log("this is OBJ: ", obj);
 
 
   return (
     <>
-      <h1>This is where your resume goes</h1> 
+      <h1>This is where your resume goes</h1>
+      {obj.map(info => (
+        <ResumeField
+          
+        />
+      ))}
     </>
   );
 }
