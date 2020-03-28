f8cece96ec5024e838aa6cda7a3602723658b3fa
svkalvakolanu <skalvakolanu@gmail.com>
2020-03-28 12:25:50 -0400

    basic next app built

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Head from "next/head";
+
+const Resume = () => (
+  <div className="home">
+    <Head>
+      <title>Srikar Kalvakolanu - Resume</title>
+    </Head>
+
+
+    <style jsx>{`
+
+    `}</style>
+  </div>
+);
+
+export default Resume;
