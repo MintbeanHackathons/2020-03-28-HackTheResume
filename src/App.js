e4162e240e9b4caa873e07272d5e3f4f309105b2
Tammie Chung <tammie.chung11@gmail.com>
2020-03-28 13:05:06 -0400

    Add conditional render for all sections
    for customizeable JSONs in future - needs testing

--- src/App.js
+++ src/App.js
@@ -11,7 +11,16 @@ function App() {
 
   return (
     <div className="App">
-
+      {resume.basics && <Basics data={resume.basics} />}
+      {resume.work && <Work data={resume.work} />}
+      {resume.volunteer && <Volunteer data={resume.volunteer} />}
+      {resume.education && <Education data={resume.education} />}
+      {resume.awards && <Awards data={resume.awards} />}
+      {resume.publications && <Publications data={resume.publications} />}
+      {resume.skills && <Skills data={resume.skills} />}
+      {resume.languages && <Languages data={resume.languages} />}
+      {resume.interests && <Interests data={resume.interests} />}
+      {resume.references && <References data={resume.references} />}
     </div>
   );
 }
