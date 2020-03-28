4555dac562a8a1ef51f660af790269bbf012919a
Sara Laffin <sarala@umich.edu>
2020-03-28 13:12:46 -0400

    set up color blocks to start filling in and add bootstrap css link

--- script.js
+++ script.js
@@ -1,5 +1,107 @@
 let my_resume = {
-  name: "Sara Laffin"
+  basics: {
+    name: "John Doe",
+    label: "Programmer",
+    picture: "",
+    email: "john@gmail.com",
+    phone: "(912) 555-4321",
+    website: "http://johndoe.com",
+    summary: "A summary of John Doe...",
+    location: {
+      address: "2712 Broadway St",
+      postalCode: "CA 94115",
+      city: "San Francisco",
+      countryCode: "US",
+      region: "California"
+    },
+    profiles: [
+      {
+        network: "Twitter",
+        username: "john",
+        url: "http://twitter.com/john"
+      }
+    ]
+  },
+  work: [
+    {
+      company: "Company",
+      position: "President",
+      website: "http://company.com",
+      startDate: "2013-01-01",
+      endDate: "2014-01-01",
+      summary: "Description...",
+      highlights: ["Started the company"]
+    }
+  ],
+  volunteer: [
+    {
+      organization: "Organization",
+      position: "Volunteer",
+      website: "http://organization.com/",
+      startDate: "2012-01-01",
+      endDate: "2013-01-01",
+      summary: "Description...",
+      highlights: ["Awarded 'Volunteer of the Month'"]
+    }
+  ],
+  education: [
+    {
+      institution: "University",
+      area: "Software Development",
+      studyType: "Bachelor",
+      startDate: "2011-01-01",
+      endDate: "2013-01-01",
+      gpa: "4.0",
+      courses: ["DB1101 - Basic SQL"]
+    }
+  ],
+  awards: [
+    {
+      title: "Award",
+      date: "2014-11-01",
+      awarder: "Company",
+      summary: "There is no spoon."
+    }
+  ],
+  publications: [
+    {
+      name: "Publication",
+      publisher: "Company",
+      releaseDate: "2014-10-01",
+      website: "http://publication.com",
+      summary: "Description..."
+    }
+  ],
+  skills: [
+    {
+      name: "Web Development",
+      level: "Master",
+      keywords: ["HTML", "CSS", "Javascript"]
+    }
+  ],
+  languages: [
+    {
+      language: "English",
+      fluency: "Native speaker"
+    }
+  ],
+  interests: [
+    {
+      name: "Wildlife",
+      keywords: ["Ferrets", "Unicorns"]
+    }
+  ],
+  references: [
+    {
+      name: "Jane Doe",
+      reference: "Reference..."
+    }
+  ]
 };
 
-document.querySelector("h1").innerHTML = my_resume.name;
+document.querySelector("h1").innerHTML = my_resume.basics.name;
+
+// var node = document.createElement("LI"); // Create a <li> node
+// var textnode = document.createTextNode("Water"); // Create a text node
+// node.appendChild(textnode); // Append the text to <li>
+// document.getElementById("myList").appendChild(node); // Append <li> to <ul> with id="myList"
