e4d0517b3c094f56a4c249bef18b36dc3c035a18
Tammie Chung <tammie.chung11@gmail.com>
2020-03-28 15:09:50 -0400

    Add blank spaces

--- src/App.js
+++ src/App.js
@@ -12,15 +12,19 @@ function App() {
       {resume.work && <Experience data={resume.work} type='work' orgType='company' title='Work Experience' />}
       {resume.volunteer && <Experience data={resume.volunteer} type='volunteer' orgType='organization' title='Volunteer' />}
       {resume.education && <Experience data={resume.education} type='education' orgType='institution' title='Education' />}
+      
       <div className='app-row'>
         {resume.awards && <Awards data={resume.awards} />}
         {resume.publications && <Publications data={resume.publications} />}
       </div>
+      
       {resume.skills && <Skills data={resume.skills} />}
+      
       <div className='app-row'>
         {resume.languages && <Languages data={resume.languages} />}
         {resume.interests && <Interests data={resume.interests} />}
       </div>
+      
       {resume.references && <References data={resume.references} />}
     </div>
   );
