33ec2f39f982946f51a5224acd2a755b4422fdb8
7figureswagger <swistek.labs@gmail.com>
2020-03-28 10:28:13 -0700

    created project, created files for the component and json data.

---
+++ resumebuilder/src/components/Resume.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import applicant from '../resume/applicant'
+
+
+const Resume = (props) => {
+  return (
+    <div>
+      {props.app}
+    </div>
+  );
+}
+
+export default Resume;
