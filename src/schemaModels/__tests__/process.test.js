43fb103568ca30a5d0b5e4c42e58bd40763ca8f2
andrew hsieh <andrewhsieh1208@gmail.com>
2020-03-28 13:17:16 -0400

    created resume class, generate resume function that makes schema models for work, volunteer, basics

--- src/schemaModels/__tests__/process.test.js
+++ src/schemaModels/__tests__/process.test.js
@@ -1,4 +1,8 @@
-import { processJson } from "../process"
+import { processJson, generateResume } from "../process"
+import Resume from "../Resume"
+import Basics from "../Basics"
+import Volunteer from "../Volunteer"
+import Work from "../Work"
 
 describe("processing the json", () => {
   it("should parse the json if valid", () => {
@@ -10,5 +14,109 @@ describe("processing the json", () => {
   it("should throw an error if the json is invalid", () => {
     const errFn = () => processJson("{")
     expect(errFn).toThrow(Error)
+  })
+})
+
+const testResume = {
+  "basics": {
+    "name": "John Doe",
+    "label": "Programmer",
+    "picture": "",
+    "email": "john@gmail.com",
+    "phone": "(912) 555-4321",
+    "website": "http://johndoe.com",
+    "summary": "A summary of John Doe...",
+    "location": {
+      "address": "2712 Broadway St",
+      "postalCode": "CA 94115",
+      "city": "San Francisco",
+      "countryCode": "US",
+      "region": "California"
+    },
+    "profiles": [{
+      "network": "Twitter",
+      "username": "john",
+      "url": "http://twitter.com/john"
+    }]
+  },
+  "work": [{
+    "company": "Company",
+    "position": "President",
+    "website": "http://company.com",
+    "startDate": "2013-01-01",
+    "endDate": "2014-01-01",
+    "summary": "Description...",
+    "highlights": [
+      "Started the company"
+    ]
+  }],
+  "volunteer": [{
+    "organization": "Organization",
+    "position": "Volunteer",
+    "website": "http://organization.com/",
+    "startDate": "2012-01-01",
+    "endDate": "2013-01-01",
+    "summary": "Description...",
+    "highlights": [
+      "Awarded 'Volunteer of the Month'"
+    ]
+  }],
+  "education": [{
+    "institution": "University",
+    "area": "Software Development",
+    "studyType": "Bachelor",
+    "startDate": "2011-01-01",
+    "endDate": "2013-01-01",
+    "gpa": "4.0",
+    "courses": [
+      "DB1101 - Basic SQL"
+    ]
+  }],
+  "awards": [{
+    "title": "Award",
+    "date": "2014-11-01",
+    "awarder": "Company",
+    "summary": "There is no spoon."
+  }],
+  "publications": [{
+    "name": "Publication",
+    "publisher": "Company",
+    "releaseDate": "2014-10-01",
+    "website": "http://publication.com",
+    "summary": "Description..."
+  }],
+  "skills": [{
+    "name": "Web Development",
+    "level": "Master",
+    "keywords": [
+      "HTML",
+      "CSS",
+      "Javascript"
+    ]
+  }],
+  "languages": [{
+    "language": "English",
+    "fluency": "Native speaker"
+  }],
+  "interests": [{
+    "name": "Wildlife",
+    "keywords": [
+      "Ferrets",
+      "Unicorns"
+    ]
+  }],
+  "references": [{
+    "name": "Jane Doe",
+    "reference": "Reference..."
+  }]
+}
+
+describe("generating the resume", () => {
+  it("should create a resume", () => {
+    const resume = generateResume(testResume)
+    expect(resume instanceof Resume).toBe(true)
+    expect(resume.basics instanceof Basics).toBe(true)
+    expect(resume.work instanceof Work).toBe(true)
+    expect(resume.volunteer instanceof Volunteer).toBe(true)
   })
 })
--- src/schemaModels/process.js
+++ src/schemaModels/process.js
@@ -1,7 +1,40 @@
+import Resume from "./Resume"
+import Basics from "./Basics"
+import Volunteer from "./Volunteer"
+import Work from "./Work"
+
 export const processJson = (json) => {
   try {
     return JSON.parse(json)
   } catch(e) {
     throw e
   }
+}
+
+export const generateResume = (rawData) => {
+  const {
+    education,
+    awards,
+    publications,
+    skills,
+    languages,
+    interests,
+    references
+  } = rawData
+  const basics = Basics.from(rawData.basics)
+  const volunteer = Volunteer.from(rawData.volunteer)
+  const work = Work.from(rawData.work)
+
+  return new Resume(
+    basics,
+    work,
+    volunteer,
+    education,
+    awards,
+    publications,
+    skills,
+    languages,
+    interests,
+    references
+  )
 }
