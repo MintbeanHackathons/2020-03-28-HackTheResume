833bc8f4f47ecc9287d26ceec9f8a35370c51af7
Bashir <b_egeh@hotmail.com>
2020-04-06 13:31:08 -0400

    add key value to each .map function; add voluteer section; add highlights section to work section

--- src/Resume.js
+++ src/Resume.js
@@ -7,7 +7,7 @@ export default function DataEntry(props) {
     const {name, label, email, phone, summary} = resume.basics;
     const {address, city, region, countryCode, postalCode} = resume.basics.location;
     const profiles = resume.basics.profiles;
-    const work = resume.work;
+    const {work, volunteer, education, awards, skills, publications, references, languages} = resume;
 
   return (
   <div>
@@ -18,26 +18,52 @@ export default function DataEntry(props) {
     <p><u>Email</u>: {email}</p>
     <p><u>Phone</u>: {phone}</p>
     {
-      profiles.map(profile=>(
-        <p><u>{profile.network}</u>: <a href={profile.url}>{profile.username}</a> </p>
+      profiles.map((profile, index)=>(
+        <p key={index}><u>{profile.network}</u>: <a href={profile.url}>{profile.username}</a> </p>
       ))
     }
     <br></br>
     <h2><strong>Summary</strong></h2>
     <p>{summary}</p>
     <br></br>
-    <div className='work'>
+    <div className='section'>
       <h2><strong>Work Experience</strong></h2>
       {
-        work.map(job=>(
-          <div>
+        work.map((job, index)=>(
+          <div key={index}>
             <p>Company: {job.company}</p>
             <p>Position: {job.position}</p>
             <p>Website: <a href={job.website}>{job.website}</a></p>
             <p>Start Date: {job.startDate}</p>
             <p>End Date: {job.endDate}</p>
-            <p>Summary</p>
-          <p>{job.summary}</p>
+            <p><u>Summary</u></p>
+            <p>{job.summary}</p>
+            <p><u>Highlights</u></p>
+            {job.highlights.map((highlight, index)=>(
+              <li key={index}>{highlight}</li>
+            ))}
+          </div>
+        ))
+      }
+    </div>
+    <br></br>
+    <div className='section'>
+      <h2><strong>Volunteer Experience</strong></h2>
+      {
+        volunteer.map((job, index)=>(
+          <div key={index}>
+            <p>Organization: {job.organization}</p>
+            <p>Position: {job.position}</p>
+            <p>Website: <a href={job.website}>{job.website}</a></p>
+            <p>Start Date: {job.startDate}</p>
+            <p>End Date: {job.endDate}</p>
+            <p><u>Summary</u></p>
+            <p>{job.summary}</p>
+            <p><u>Highlights</u></p>
+            {job.highlights.map((highlight, index)=>(
+              <li key={index}>{highlight}</li>
+            ))}
+
           </div>
         ))
       }
